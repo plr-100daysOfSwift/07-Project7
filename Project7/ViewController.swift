7db6ad2826546ebbe658c4e06a034e7aa685d8df
Paul Richardson <developer@photographerafoot.com>
2021-04-16 08:20:53 +1000

    CHG: Implement cellForRowAt
    placeholder text

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -22,5 +22,12 @@ class ViewController: UITableViewController {
 		return petitions.count
 	}
 
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		cell.textLabel?.text = "Title goes here"
+		cell.detailTextLabel?.text = "Subtitle goes here"
+		return cell
+	}
+	
 }
 

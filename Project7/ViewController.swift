6d763bc3dedd7bb6e243442c4962bd860cbede00
Paul Richardson <developer@photographerafoot.com>
2021-04-16 08:16:40 +1000

    CHG: add a property to store the data

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+	var petitions = [String]()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.

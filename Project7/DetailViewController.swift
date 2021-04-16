ad37fd76b90789a178bbc86dc4407180aef0c193
Paul Richardson <developer@photographerafoot.com>
2021-04-17 08:01:00 +1000

    CHG: add property to hold petition; load web view

--- Project7/DetailViewController.swift
+++ Project7/DetailViewController.swift
@@ -6,24 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	var webView: WKWebView!
+	var detailItem: Petition?
 
-        // Do any additional setup after loading the view.
-    }
-    
+	override func loadView() {
+		webView = WKWebView()
+		view = webView
+	}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
 
 }
